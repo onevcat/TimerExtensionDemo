a9a6f0576f3f39784981b0350c4b284df7601ce6
onevcat <onevcat@gmail.com>
2014-08-01 14:04:36 +0900

    Init commit

---
+++ SimpleTimer/SimpleTimer/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SimpleTimer
+//
+//  Created by 王 巍 on 14-8-1.
+//  Copyright (c) 2014年 OneV's Den. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    @IBOutlet weak var lblTimer: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnStartPressed(sender: AnyObject) {
+
+    }
+    
+    @IBAction func btnStopPressed(sender: AnyObject) {
+
+    }
+
+}
+
