708b4c817a3cff37722f60b7bbc84f75c3e30bd2
onevcat <onevcat@gmail.com>
2014-08-01 17:19:38 +0900

    Basic functional

---
+++ SimpleTimer/SimpleTimer/Timer.swift
@@ -0,0 +1,112 @@
+//
+//  Timer.swift
+//  SimpleTimer
+//
+//  Created by 王 巍 on 14-8-1.
+//  Copyright (c) 2014年 OneV's Den. All rights reserved.
+//
+
+import UIKit
+
+let timerErrorDomain = "SimpleTimerError"
+
+enum SimperTimerError: Int {
+    case AlreadyRunning = 1001
+    case NegativeLeftTime = 1002
+    case NotRunning = 1003
+}
+
+extension NSTimeInterval {
+    func toString() -> String {
+        let totalSecond = Int(self)
+        let minute = totalSecond / 60
+        let second = totalSecond % 60
+        
+        switch (minute, second) {
+        case (0...9, 0...9):
+            return "0\(minute):0\(second)"
+        case (0...9, _):
+            return "0\(minute):\(second)"
+        case (_, 0...9):
+            return "\(minute):0\(second)"
+        default:
+            return "\(minute):\(second)"
+        }
+    }
+}
+
+class Timer: NSObject {
+    
+    var running: Bool = false
+    
+    var leftTime: NSTimeInterval {
+    didSet {
+        if leftTime < 0 {
+            leftTime = 0
+        }
+    }
+    }
+    
+    var leftTimeString: String {
+    get {
+        return leftTime.toString()
+    }
+    }
+    
+    private var timerTickHandler: (NSTimeInterval -> ())? = nil
+    private var timerStopHandler: (Bool ->())? = nil
+    private var timer: NSTimer!
+    
+    init(timeInteral: NSTimeInterval) {
+        leftTime = timeInteral
+    }
+    
+    func start(#updateTick: (leftTick: NSTimeInterval -> Void)?, stopHandler: (finished: Bool -> Void)?) -> (start: Bool, error: NSError?) {
+        if running {
+            return (false, NSError(domain: timerErrorDomain, code: SimperTimerError.AlreadyRunning.toRaw(), userInfo:nil))
+        }
+        
+        if leftTime < 0 {
+            return (false, NSError(domain: timerErrorDomain, code: SimperTimerError.NegativeLeftTime.toRaw(), userInfo:nil))
+        }
+        
+        timerTickHandler = updateTick
+        timerStopHandler = stopHandler
+        
+        running = true
+        
+        NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector:"countTick", userInfo: nil, repeats: true)
+        
+        return (true, nil)
+    }
+    
+    func stop() -> (start: Bool, error: NSError?) {
+        if !running {
+            return (false, NSError(domain: timerErrorDomain, code: SimperTimerError.NotRunning.toRaw(), userInfo:nil))
+        }
+        
+        running = false
+        timer.invalidate()
+        timer = nil
+        
+        if let stopHandler = timerStopHandler {
+            stopHandler(true)
+        }
+        
+        timerStopHandler = nil
+        timerTickHandler = nil
+        
+        return (true, nil)
+    }
+    
+    @objc private func countTick() {
+        leftTime = leftTime - 1
+        if leftTime > 0 {
+            if let tickHandler = timerTickHandler {
+                tickHandler(leftTime)
+            }
+        } else {
+            stop()
+        }
+    }
+}
--- SimpleTimer/SimpleTimer/ViewController.swift
+++ SimpleTimer/SimpleTimer/ViewController.swift
@@ -8,26 +8,51 @@
 
 import UIKit
 
+let defaultTimeInterval: NSTimeInterval = 1 * 60
+
 class ViewController: UIViewController {
                             
     @IBOutlet weak var lblTimer: UILabel!
     
+    var timer: Timer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func updateLabel() {
+        lblTimer.text = timer.leftTimeString
+    }
 
     @IBAction func btnStartPressed(sender: AnyObject) {
-
+        if timer == nil {
+            timer = Timer(timeInteral: defaultTimeInterval)
+        }
+        
+        let (started, error) = timer.start(updateTick: {
+            [weak self] leftTick in self!.updateLabel()
+            }, stopHandler: {
+                finished in println(finished)
+            })
+        
+        if started {
+            updateLabel()
+        } else {
+            if let err = error {
+                println("error: \(err.code)")
+            }
+        }
     }
     
     @IBAction func btnStopPressed(sender: AnyObject) {
-
+        
     }
 
 }
